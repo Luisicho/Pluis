28556ebe1243c850b8a0b0dc24a125de2baeee9b
Luisicho <luism18.1999@gmail.com>
2024-09-01 14:44:50 -0700

    Se creo upload para cargar imagenes backend

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const multer = require('multer')
+const fs = require('node:fs')
 const app = express()
 const PORT = process.env.PORT || 5000;
 
@@ -7,8 +8,15 @@ const upload = multer({dest:'uploads/'})
 
 app.post('/images/single',upload.single('imagenPerfil'),(req,res) =>{
     console.log(req.file);
+    saveImage(req.file);
     res.send('Termino Imagen Function');
 })
+
+function saveImage(file){
+    const newPath = `./uploads/${file.originalname}`;
+    fs.renameSync(file.path,newPath);
+    return newPath
+}
 
 app.get('/api', (req,res) => {
     res.json({'users': ['uno','dos','tres']})
