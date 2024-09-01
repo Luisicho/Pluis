0009704a7e42d4402128ce137b5d1b4787a6e476
Luisicho <luism18.1999@gmail.com>
2024-09-01 14:35:02 -0700

    Se creo multer y prueba de post para imagenes

--- server/server.js
+++ server/server.js
@@ -1,6 +1,14 @@
 const express = require('express')
+const multer = require('multer')
 const app = express()
 const PORT = process.env.PORT || 5000;
+
+const upload = multer({dest:'uploads/'})
+
+app.post('/images/single',upload.single('imagenPerfil'),(req,res) =>{
+    console.log(req.file);
+    res.send('Termino Imagen Function');
+})
 
 app.get('/api', (req,res) => {
     res.json({'users': ['uno','dos','tres']})
